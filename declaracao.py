e395ea626641fdbf8c72a6ed6668b664a0ecff0d
Ed <33470642+edersonsousa@users.noreply.github.com>
2024-03-25 17:01:36 -0300

    botões
    ...

--- declaracao.py
+++ declaracao.py
@@ -1,8 +1,10 @@
-from tool_box import validar_nome_entry, validar_rg_entry, validar_cpf_entry, limpar_campos, toggle_check_a_partir, toggle_check_periodo_fechado, ato_box_select, lei_box_select
+from tool_box import validar_nome_entry, validar_rg_entry, validar_cpf_entry, limpar_campos, toggle_check_a_partir, toggle_check_periodo_fechado, ato_box_select, lei_box_select, jornada_box_select, on_validate, validate_content, atualizar_declara, validar_tipo_de_servidor, declaracao
 import tkinter as tk
 from tkinter.ttk import Combobox
 from tkinter import Tk, Label, Entry, Checkbutton, W, Button, messagebox, END , simpledialog
 from PIL import Image, ImageDraw
+
+
 
 window = Tk()
 window.title("Declarações")
@@ -67,9 +69,11 @@ regime_label.grid(row=12, column=1, pady = 4, sticky="W")
 nome_entry = Entry(width=45)
 nome_entry.grid(row=0, column=2)
 
-validate_cmd = window.register(lambda P, entry=nome_entry: validar_nome_entry(P, entry)) # Registra a função de validação
+validate_cmd = window.register(lambda P, entry=nome_entry: validar_nome_entry(P, entry))
 nome_entry.config(validate="key", validatecommand=(validate_cmd, "%P"))
+nome_entry.bind("<FocusOut>",lambda event: validate_content)
 nome_entry.focus()
+
 
 rg_entry = Entry(width=45)
 rg_entry.grid(row=1, column=2, pady = 4)
@@ -81,9 +85,6 @@ cpf_entry.grid(row=2, column=2, pady = 4)
 
 cpf_entry.bind("<Return>", lambda event: validar_cpf_entry(event, cpf_entry))
 cpf_entry.bind("<Tab>", lambda event: validar_cpf_entry(event, cpf_entry))
-
-#estado_civil_entry = Entry(width=45)
-#estado_civil_entry.grid(row=3, column=2, pady = 4)
 
 estado_civil_combo = Combobox(window, 
         values=["Solteiro(a)", 
@@ -97,7 +98,8 @@ ato_combo = Combobox(window,
                 "Designação com posterior Nomeação"], width=42)
 ato_combo.grid(row=4, column=2, pady = 6)
 ato_combo.bind("<<ComboboxSelected>>", lambda event: 
-        ato_box_select(event, ato_combo, a_partir_var, periodo_fechado_var, a_partir_checkbutton, periodo_fechado_checkbutton))
+        ato_box_select(event, ato_combo, a_partir_var, periodo_fechado_var, a_partir_checkbutton, periodo_fechado_checkbutton, lei_combo))
+ato_combo.bind("<FocusOut>", lambda event: validar_tipo_de_servidor(ato_combo, cargo_de_origem_entry, bnt_n_servidor, bnt_servidor))
 
 lei_combo = Combobox(window, 
         values=["Art.5º da lei complementar nº 1080/2008",
@@ -113,27 +115,137 @@ jornada_combo = Combobox(window,
                 "Jornada Parcial de Trabalho",
                 "Jornada de 30(trinta) horas de Trabalho"], width=42, state="disable")
 jornada_combo.grid(row=6, column=2)
-
-
+jornada_combo.bind("<<ComboboxSelected>>", lambda event:
+        jornada_box_select(cargo_combo, lei_combo))
 
 cargo_combo = Combobox(window, width=42, state="disable")
 
 cargo_combo.grid(row=7, column=2)
 
+ua_combo = Combobox(window, values=[
+                                        'Centro de Atenção Integrada em Saúde Mental "Philippe Pinel" - CAISM Philippe Pinel',
+                                        'Centro de Atenção Integral à Saúde "Clemente Ferreira" de Lins	',
+                                        'Centro de Atenção Integral à Saúde "Professor Cantídio de Moura Campos"',
+                                        'Centro de Atenção Integral à Saúde de Santa Rita - C.A.I.S./SR	',
+                                        'Centro de Desenvolvimento do Portador de Deficiência Mental, em Itu	',
+                                        'Centro de Reabilitação de Casa Branca	',
+                                        'Centro de Referência da Saúde da Mulher	',
+                                        #'Centro de Referência de Álcool, Tabaco e Outras Drogas',
+                                        'Centro de Referência e Treinamento - "DST/AIDS"	',
+                                        'Centro de Vigilância Epidemiológica "Professor Alexandre Vranjac"	',
+                                        'Centro de Vigilância Sanitária	',
+                                        'Centro Especializado em Reabilitação "Doutor Arnaldo Pezzuti Cavalcanti", em Mogi das Cruzes	',
+                                        'Centro Pioneiro em Atenção Psicossocial "Arquiteto Januário José Ezemplari"- CPAP	',
+                                        'Complexo Hospitalar "Padre Bento" de Guarulhos	',
+                                        'Complexo Hospitalar do Juquery, em Franco da Rocha',
+                                        'Conjunto Hospitalar de Sorocaba',
+                                        'Conjunto Hospitalar do Mandaqui',
+                                        'Coordenadoria de Defesa e Saúde Animal',
+                                        'Coordenadoria de Gestão Orçamentária e Financeira  - CGOF',
+                                        'Coordenadoria de Planejamento de Saúde	',
+                                        'Coordenadoria de Recursos Humanos	',
+                                        'Coordenadoria Geral de Administração	',
+                                        'Departamento de Gerenciamento Ambulatorial da Capital - DGAC	',
+                                        'DRS I - Grande São Paulo	',
+                                        'DRS II - Araçatuba	',
+                                        'DRS III - Araraquara	',
+                                        'DRS IV "Dr. Maurício Fang" - Baixada Santista	',
+                                        'DRS IX - Marília	',
+                                        'DRS V - Barretos	',
+                                        'DRS VI - Bauru	',
+                                        'DRS VII "Dr. Leôncio de Souza Queiroz" - Campinas',
+                                        'DRS VIII - Franca',
+                                        'DRS X  - Piracicaba	',
+                                        'DRS XI - Presidente Prudente	',
+                                        'DRS XII - Registro	',
+                                        'DRS XIII - Ribeirão Preto	',
+                                        'DRS XIV - São João da Boa Vista	',
+                                        'DRS XV - São José do Rio Preto	',
+                                        'DRS XVI - Sorocaba	',
+                                        'DRS XVII - Taubaté	',
+                                        'DRS XVIII - Botucatu	',
+                                        'Gabinete do Coordenador	',
+                                        'Gabinete do Secretário e Assessorias	',
+                                        'Grupo de Assistência Farmacêutica	',
+                                        'Grupo de Resgate - GRAU	',
+                                        'Hospital "Adhemar de Barros" em Divinolândia	',
+                                        'Hospital "Dr. Francisco Ribeiro Arantes", em Itu	',
+                                        'Hospital "Guilherme Álvaro" em Santos	',
+                                        'Hospital "Manoel de Abreu" de Bauru	',
+                                        'Hospital "Nestor Goulart Reis" em Américo Brasiliense	',
+                                        'Hospital das Clínicas "Luzia de Pinho Melo" em Mogi das Cruzes	',
+                                        'Hospital Estadual "Dr. Odilo Antunes de Siqueira" em Presidente Prudente	',
+                                        'Hospital Estadual "Dr. Oswaldo Brandi Faria" em Mirandópolis	',
+                                        'Hospital Estadual Especializado em Reabilitação "Dr. Francisco Ribeiro Arantes", em Itu	',
+                                        'Hospital Geral "Dr. Álvaro Simões de Souza" em Vila Nova Cachoeirinha	',
+                                        'Hospital Geral "Dr. José Pangella" de Vila Penteado	',
+                                        'Hospital Geral "Dr. Manoel Bifulco" em São Mateus	',
+                                        'Hospital Geral "Jesus Teixeira da Costa" em Guaianazes	',
+                                        'Hospital Geral "Prefeito Miguel Martin Gualda", de Promissão',
+                                        'Hospital Geral de Taipas	',
+                                        'Hospital Infantil "Cândido Fontoura"',
+                                        'Hospital Maternidade Interlagos "Waldemar Seyssel - Arrelia"',
+                                        'Hospital Psiquiátrico Pinel, em Pirituba',
+                                        'Hospital Regional "Doutor Leopoldo Bevilacqua" do Vale do Ribeira, em Pariquera-Açu	',
+                                        'Hospital Regional "Dr. Osíris Florindo Coelho" em Ferraz de Vasconcelos	',
+                                        'Hospital Regional "Dr. Vivaldo Martins Simões" em Osasco	',
+                                        'Hospital Regional de Assis	',
+                                        'Hospital Regional Sul	',
+                                        'Hospital Santa Tereza em Ribeirão Preto	',
+                                        'Instituto "Adolfo Lutz" - IAL	',
+                                        'Instituto "Dante Pazzanese" de Cardiologia	',
+                                        'Instituto "Lauro de Souza Lima" em Bauru	',
+                                        'Instituto Butantan	',
+                                        'Instituto Clemente Ferreira - ICF',
+                                        'Instituto de Infectologia "Emílio Ribas"',
+                                        'Instituto de Saúde',
+                                        'Instituto Pasteur',
+                                        'Instituto Paulista de Geriatria e Gerontologia - IPGG "José Ermírio de Moraes"',
+                                        'Núcleo de Gestão Assistencial 14 - Campos Elíseos',
+                                        'Unidade de Gestão Assistencial I',
+                                        'Unidade de Gestão Assistencial II	',
+                                        'Unidade de Gestão Assistencial III	',
+                                        'Unidade de Gestão Assistencial IV	',
+                                        'Unidade de Gestão Assistencial V	'
+                                        
+                                
+                                        ], width=42)
+ua_combo.grid(row=9, column=2, pady=6)
+
 destinacao_entry = Entry(width=45)
 destinacao_entry.grid(row=8, column=2)
 
-ua_entry = Entry(width=45)
-ua_entry.grid(row=9, column=2)
-
-coordenadoria_entry = Entry(width=45)
-coordenadoria_entry.grid(row=10, column=2)
+coordenadoria_combo = Combobox(window, 
+                        values = [
+                                "Administração Superior da Secretaria e da Sede	"                               ,
+                                "Coordenadoria de Assistência Farmacêutica	"                               ,
+                                "Coordenadoria de Ciência, Tecnologia e Insumos Estratégicos de Saúde	"       ,
+                                "Coordenadoria de Controle de Doenças	"                                       ,
+                                "Coordenadoria de Defesa e Saúde Animal	"                                       ,
+                                "Coordenadoria de Gestão de Contratos de Serviços de Saúde	"               ,
+                                "Coordenadoria de Gestão Orçamentaria e Financeira	"                       ,
+                                "Coordenadoria de Planejamento de Saúde	"                                       ,
+                                "Coordenadoria de Regiões de Saúde	"                                       ,
+                                "Coordenadoria de Serviços de Saúde	"                                       ,
+                                "Coordenadoria Geral de Administração	"
+                                ],
+                               width=42)
+coordenadoria_combo.grid(row=10, column=2)
 
 cargo_de_origem_entry = Entry(width=45)
 cargo_de_origem_entry.grid(row=11, column=2)
+cargo_de_origem_entry.bind("<FocusOut>", lambda event: validar_tipo_de_servidor(ato_combo, cargo_de_origem_entry, bnt_n_servidor, bnt_servidor))
 
-cargo_entry = Entry(width=45)
-cargo_entry.grid(row=12, column=2)
+
+regime_combo = Combobox(window, 
+                        values = [
+                                "Efetivo"       ,
+                                "Lei 500"       ,
+                                "Comissão"      ,
+                                "CLT"           
+                                ],
+                               width=42)
+regime_combo.grid(row=12, column=2)
 
 a_partir_var = tk.BooleanVar()
 a_partir_checkbutton = Checkbutton(window, text="A partir", variable=a_partir_var, font="Montserrat", 
@@ -145,24 +257,24 @@ periodo_fechado_checkbutton = Checkbutton(window, text="Período Fechado", varia
                                    command=lambda: toggle_check_periodo_fechado(periodo_fechado_var, a_partir_var, a_partir_checkbutton, ato_combo))
 periodo_fechado_checkbutton.grid(row=3, sticky=tk.W, column=5, columnspan=4)
 
-add_button = Button(text="Gerar Dados \n Não Servidor", width=15, bg="cyan", state="disable")
-add_button["font"] = ("Montserrat","12")
-add_button.grid(row=24, column=0, columnspan=2)
+bnt_n_servidor = Button(text="Gerar Dados \n Não Servidor", width=15, bg="cyan", state="disable" , 
+        command=lambda: declaracao(declara = {"Nome": nome_entry.get()}))
+bnt_n_servidor["font"] = ("Montserrat","12")
+bnt_n_servidor.grid(row=24, column=0, columnspan=2)
 
-add_button = Button(text="Gerar Dados \n Servidor", width=15, bg="cyan", state="disable")
-add_button["font"] = ("Montserrat","12")
-add_button.grid(row=24, column=2, columnspan=2)
+bnt_servidor = Button(text="Gerar Dados \n Servidor", width=15, bg="cyan", state="disable")
+bnt_servidor["font"] = ("Montserrat","12")
+bnt_servidor.grid(row=24, column=2, columnspan=2)
 
-add_button_limpar = Button(text="Limpar", width=15, bg="gray", 
+bnt_limpar = Button(text="Limpar", width=15, bg="gray", 
         command=lambda: limpar_campos(nome_entry, rg_entry, cpf_entry, estado_civil_combo, 
-                                      ato_combo, jornada_combo, lei_combo, cargo_entry, destinacao_entry, 
-                                      ua_entry, coordenadoria_entry, cargo_de_origem_entry, 
-                                      a_partir_var, a_partir_checkbutton, periodo_fechado_var, periodo_fechado_checkbutton))
-add_button_limpar["font"] = ("Montserrat","12")
-add_button_limpar.grid(row=24, column=4, columnspan=2, pady=25, padx=25)
+                                      ato_combo, jornada_combo, lei_combo, cargo_combo, destinacao_entry, 
+                                      ua_combo, coordenadoria_combo, cargo_de_origem_entry, 
+                                      a_partir_var, a_partir_checkbutton, periodo_fechado_var, periodo_fechado_checkbutton, regime_combo))
+bnt_limpar["font"] = ("Montserrat","12")
+bnt_limpar.grid(row=24, column=4, columnspan=2, pady=25, padx=25)
 
-add_button_sair = Button(text="SAIR", width=15, bg="red", command=window.quit)
-add_button_sair["font"] = ("Montserrat","12")
-add_button_sair.grid(row=24, column=8, columnspan=2)
-
+bnt_sair = Button(text="SAIR", width=15, bg="red", command=window.quit)
+bnt_sair["font"] = ("Montserrat","12")
+bnt_sair.grid(row=24, column=8, columnspan=2)
 window.mainloop()
--- tool_box.py
+++ tool_box.py
@@ -5,16 +5,41 @@ from PIL import Image, ImageDraw
 import re
 
 def validar_nome_entry(new_value, nome_entry):
-    # Verifica se a primeira letra é maiúscula, se não for, converte para maiúscula
-    if new_value and not new_value[0].isupper():
-        new_value = new_value.capitalize()
-        nome_entry.delete(0, "end")
-        nome_entry.insert(0, new_value)
+    # Verifica se a entrada está vazia
+    if not new_value:
+        return True
+    
+    # Capitaliza a primeira letra de cada palavra na string
+    new_value = new_value.title()
+
     # Verifica se a entrada contém apenas letras e espaços
     if re.match(r'^[a-zA-Z\s]+$', new_value):
         return True
     else:
+        if any(char.isdigit() for char in new_value):
+            messagebox.showerror("Erro", "A entrada não pode conter números.")
+        else:
+            messagebox.showerror("Erro", "A entrada deve conter apenas letras e espaços.")
+        nome_entry.delete(0, "end")
         return False
+
+def on_validate(P, nome_entry):
+    return validar_nome_entry(P, nome_entry)
+
+
+def validate_content(event):
+    content = nome_entry.get()
+    validar_nome_entry(content, nome_entry)
+    atualizar_declara()
+    
+def atualizar_declara():
+    global declara
+    declara["nome"] = nome_entry.get()
+    declara["rg_entry"] = rg_entry.get()
+    # Adicione outras variáveis conforme necessário
+
+    # Exibir os valores armazenados no dicionário de variáveis
+    return(declara)
 
     
 def validar_rg_entry(new_value, rg_entry):
@@ -37,7 +62,6 @@ def validar_rg_entry(new_value, rg_entry):
         return True
     else:
         return False
-
 
 
 def validar_cpf_entry(event):
@@ -65,35 +89,34 @@ def validar_cpf_entry(event):
     else:
         return False
 
-
-
-
-
-
-def limpar_campos(nome_entry, rg_entry, cpf_entry, estado_civil_combo, ato_combo, jornada_combo, lei_combo, cargo_entry, 
-                  destinacao_entry, ua_entry, coordenadoria_entry, cargo_de_origem_entry, 
-                  a_partir_var, a_partir_checkbutton, periodo_fechado_var, periodo_fechado_checkbutton):
+def limpar_campos(nome_entry, rg_entry, cpf_entry, estado_civil_combo, ato_combo, jornada_combo, lei_combo, cargo_combo, 
+                  destinacao_entry, ua_combo, coordenadoria_combo, cargo_de_origem_entry, a_partir_var, a_partir_checkbutton, periodo_fechado_var, 
+                  periodo_fechado_checkbutton, regime_combo):
     # Limpa o valor de todos os campos de entrada
     nome_entry.delete(0, END)
+    nome_entry.focus()
     rg_entry.delete(0, END)
     cpf_entry.delete(0, END)
     estado_civil_combo.set('')
     ato_combo.set('')
     jornada_combo.set('')
-    ato_combo["values"] = "Nomeação","Designação","Designação com posterior Nomeação"
+    jornada_combo.config(state="disable")
+    #ato_combo["values"] = "Nomeação","Designação","Designação com posterior Nomeação"
+    #ato_combo.config(state="disable")
     lei_combo.set('')
-    cargo_entry.delete(0, END)
+    #lei_combo.config(state="disable")
+    cargo_combo.set('')
+    cargo_combo.config(state="disable")
     destinacao_entry.delete(0, END)
-    ua_entry.delete(0, END)
-    coordenadoria_entry.delete(0, END)
+    ua_combo.set('')
+    coordenadoria_combo.set('')
     cargo_de_origem_entry.delete(0, END)
     periodo_fechado_var.set(False)
     a_partir_var.set(False)
     a_partir_checkbutton.config(state="normal")
     periodo_fechado_checkbutton.config(state="normal")
-    nome_entry.delete(0, END)
-    nome_entry.focus()
-    nome_entry.delete(0, END)
+    regime_combo.set('')
+ 
 
 
 def toggle_check_a_partir(a_partir_var, periodo_fechado_var, periodo_fechado_checkbutton, ato_combo):
@@ -104,7 +127,7 @@ def toggle_check_a_partir(a_partir_var, periodo_fechado_var, periodo_fechado_che
         user_date = simpledialog.askstring("Data", "Qual a data do a partir? Ex:(dd/mm/aaaa)")
         # Verificar se a data está no formato correto
         if user_date and len(user_date) == 10 and user_date[2] == '/' and user_date[5] == '/':
-            print("Data selecionada:", user_date)
+            #print("Data selecionada:", user_date)
             # Use a data fornecida pelo usuário como desejar
             # Atualizar valores do Combobox
             ato_combo["values"] = "Designação", "Designação com posterior Nomeação" 
@@ -112,7 +135,7 @@ def toggle_check_a_partir(a_partir_var, periodo_fechado_var, periodo_fechado_che
             
         else:
             tk.messagebox.showerror("Erro", "Formato de data inválido. Por favor, insira uma data no formato dd/mm/aaaa.")
-            toggle_check_a_partir(var, periodo_fechado_var, periodo_fechado_checkbutton, ato_combo)
+            toggle_check_a_partir(a_partir_var, periodo_fechado_var, periodo_fechado_checkbutton, ato_combo)
     else:
         periodo_fechado_checkbutton.config(state="normal")
     
@@ -129,27 +152,114 @@ def toggle_check_periodo_fechado(periodo_fechado_var, a_partir_var, a_partir_che
         a_partir_checkbutton.config(state="normal")
 
 
-def ato_box_select(event, ato_combo, a_partir_var, periodo_fechado_var, a_partir_checkbutton, periodo_fechado_checkbutton):
+def ato_box_select(event, ato_combo, a_partir_var, periodo_fechado_var, a_partir_checkbutton, periodo_fechado_checkbutton, lei_combo):
     selected_value = ato_combo.get()
     if selected_value == "Nomeação":
         a_partir_var.set(False)
         periodo_fechado_var.set(False)
         a_partir_checkbutton.config(state=tk.DISABLED)
         periodo_fechado_checkbutton.config(state=tk.DISABLED)
+        lei_combo.config(state="normal")
+        
     elif selected_value == "Designação com posterior Nomeação":
         periodo_fechado_var.set(False)
         a_partir_checkbutton.config(state=tk.NORMAL)
         periodo_fechado_checkbutton.config(state=tk.DISABLED)
+        lei_combo.config(state="normal")
+
     else:
         a_partir_checkbutton.config(state=tk.NORMAL)
         periodo_fechado_checkbutton.config(state=tk.NORMAL)
+        lei_combo.config(state="normal")
+
 
 
 def lei_box_select(lei_combo, jornada_combo):
     selected_value = lei_combo.get()
     if selected_value == "Art.5º da lei complementar nº 1080/2008":
-        jornada_combo["values"] = "Jornada Completa de Trabalho"
+        jornada_combo["values"] = ["Jornada Completa de Trabalho"]
         jornada_combo.set("Jornada Completa de Trabalho")
+        jornada_combo.config(state=tk.NORMAL)
+        
     elif selected_value == "Art.8º da lei complementar nº 1157/2011":
+        jornada_combo.config(state=tk.NORMAL)
+        jornada_combo.set("")
         jornada_combo["values"] = "Jornada Básica de Trabalho", "Jornada Parcial de Trabalho", "Jornada de 30(trinta) horas de Trabalho"
         jornada_combo.focus()
+        
+def jornada_box_select(cargo_combo, lei_combo):
+    selected_value = lei_combo.get()
+    if selected_value == "Art.5º da lei complementar nº 1080/2008":
+        cargo_combo.set("")
+        cargo_combo["values"] = [
+                                "Assessor de Gabinete I"                , 
+                                "Assessor de Gabinete II"               , 
+                                "Assessor I"                            ,
+                                "Assessor Técnico de Coordenador"       ,
+                                "Assessor Técnico de Gabinete I"        ,
+                                "Assessor Técnico de Gabinete II"       ,
+                                "Assessor Técnico de Gabinete III"      ,
+                                "Assessor Técnico de Gabinete IV"       ,
+                                "Assessor Técnico I"	                ,
+                                "Assessor Técnico II"   	            ,
+                                "Assessor Técnico III"	                ,
+                                "Assessor Técnico IV"   	            ,
+                                "Assessor Técnico V"    	            ,
+                                "Chefe de Gabinete"     	            ,
+                                "Chefe I"   	                        ,
+                                "Chefe II"      	                    ,
+                                "Diretor I"	                            ,
+                                "Diretor II"    	                    ,
+                                "Diretor III"                           ,
+                                "Diretor Técnico I"	                    ,
+                                "Diretor Técnico II"    	            ,
+                                "Diretor Técnico III"   	            ,
+                                "Encarregado I"     	                ,
+                                "Encarregado II"    	                ,
+                                "Supervisor Técnico I"           
+                                ]
+        cargo_combo.config(state=tk.NORMAL)
+        cargo_combo.set("")
+
+    elif selected_value == "Art.8º da lei complementar nº 1157/2011":
+        cargo_combo.set("")
+        cargo_combo["values"] = [
+                                "Assessor Técnico de Coordenador de Saúde"  ,
+                                "Assessor Técnico em Saúde Pública I"       ,
+                                "Assessor Técnico em Saúde Pública II"      ,
+                                "Assessor Técnico em Saúde Pública III"     ,
+                                "Chefe de Saúde I"                          ,
+                                "Chefe de Saúde II"                         ,
+                                "Cirurgião Dentista Sanitarista Inspetor"   ,
+                                "Coordenador de Saúde"                      ,
+                                "Diretor Técnico de Saúde I"                ,
+                                "Diretor Técnico de Saúde II"               ,
+                                "Diretor Técnico de Saúde III"              ,
+                                "Encarregado de Saúde I"                    ,
+                                "Encarregado de Saúde II"                   ,
+                                "Enfermeiro Inspetor de Saúde Pública"      ,
+                                "Engenheiro Sanitarista Assessor"           ,
+                                "Médico Inspetor"                           ,
+                                "Supervisor de Equipe Técnica de Saúde"     ,
+                                "Supervisor de Saúde"         
+                                ]
+        cargo_combo.config(state=tk.NORMAL)
+        cargo_combo.set("")
+
+
+        
+def validar_tipo_de_servidor(ato_combo, cargo_de_origem_entry, bnt_n_servidor, bnt_servidor):
+    if (ato_combo.get() == "Nomeação" and cargo_de_origem_entry.get() == ""):
+        bnt_n_servidor.config(state="normal")
+        bnt_servidor.config(state="disable")
+    elif (ato_combo.get() != "Nomeação" or cargo_de_origem_entry.get() != ""):
+        bnt_n_servidor.config(state="disable")
+        bnt_servidor.config(state="normal")
+    
+    #print(ato_combo.get())
+    #print(cargo_de_origem_entry.get())
+    
+def declaracao(declara):
+    print (declara)
+    declara.values()
+    print(declara.values())
